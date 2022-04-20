4532bc3885fef2745341cea3c42d918dfa7063bc
walidesoussi <eoussi.walid@gmail.com>
2022-04-20 14:37:09 +0100

    First commit

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data ={
+
+    products :[
+        {
+            name:'Brique A03',
+            slug : 'Brique-A03',
+            category :'Briques',
+            image:'./images/b3.jpg',
+            price:0.235,
+            countInStock:1000,
+            rating:4.5,
+            description:'Brique Rouges en terre cuite pressées cuites en four continu',
+            
+        },
+        {
+            name:'Brique A08',
+            slug : 'Brique-A08',
+            category :'Briques',
+            image:'./images/b8.jpg',
+            price:0.674,
+            countInStock:1000,
+            rating:4.5,
+            description:'C’est une brique isolante dure et légère avec 8 alvéoles.',
+            
+        },
+        {
+            name:'Brique P08',
+            slug : 'Brique-P08',
+            category :'Briques',
+            image:'./images/b8p.jpg',
+            price:0.663,
+            countInStock:1000,
+            rating:4.5,
+            description:'C’est une brique isolante dure et légère avec 8 alvéoles. ',
+            
+        },
+        {
+            name:'Brique A12',
+            slug : 'Brique-A12',
+            category :'Briques',
+            image:'./images/b12.jpg',
+            price:0.720,
+            countInStock:0,
+            rating:4.5,
+            description:'C’est une brique dure et légère  comporte 12 alvéoles.',
+            
+        },
+        {
+            name:'Brique A35',
+            slug : 'Brique-A35',
+            category :'Briques',
+            image:'./images/b35.jpg',
+            price:1.472,
+            countInStock:1000,
+            rating:4.5,
+            description:'Brique double cloison : le creux et le format alvéolaire de la BDC permettent une diminution importante des ponts thermiques',
+            
+        },
+        {
+            name:'BRIQUE HOURD 16',
+            slug : 'BRIQUE-HOURD-16',
+            category :'Briques',
+            image:'./images/bh16.jpg',
+            price:1.105,
+            countInStock:1000,
+            rating:4.5,
+            description:'L’hourdi est un matériau à la fois noble et d un usage pratique pour la réalisation des dalles.',
+            
+        },
+        {
+            name:'BRIQUE HOURD 19',
+            slug : 'BRIQUE-HOURD-19',
+            category :'Briques',
+            image:'./images/bh19.jpg',
+            price:1.215,
+            countInStock:1000,
+            rating:4.5,
+            description:'L’hourdi est un matériau à la fois noble et d un usage pratique pour la réalisation des dalles.',
+            
+        },
+      
+    ],
+};
+export default data ;
